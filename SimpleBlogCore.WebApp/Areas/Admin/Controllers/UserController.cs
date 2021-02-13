8506451c996c47f102067d7e64a16d3625bead3d
Zeroox <>
2021-02-13 19:53:15 +0500

    Added viewing of the list of users with the ability to change their role

---
+++ SimpleBlogCore.WebApp/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimpleBlogCore.Domain.Entities;
+using SimpleBlogCore.WebApp.Areas.Admin.Models;
+using System;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SimpleBlogCore.WebApp.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Authorize(Roles = "Admin")]
+	public class UserController : Controller
+	{
+		private readonly UserManager<User> userManager;
+
+		public UserController(UserManager<User> userManager)
+		{
+			this.userManager = userManager;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public async Task<IActionResult> GetAll()
+		{
+			var users = (await userManager.Users.ToListAsync())
+				.Select(async u => new UserViewModel(u, await userManager.GetRolesAsync(u)))
+				.Select(t => t.Result);
+
+			return Json(new { data = users.ToArray() }, new JsonSerializerOptions {
+				WriteIndented = true,
+			});
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> RoleToggle(Guid userId)
+		{
+			var user = await userManager.FindByIdAsync(userId.ToString());
+			if (await userManager.IsInRoleAsync(user, role: "Admin"))
+			{
+				await userManager.RemoveFromRoleAsync(user, role: "Admin");
+				await userManager.AddToRoleAsync(user, role: "User");
+			}
+			else
+			{
+				await userManager.RemoveFromRoleAsync(user, role: "User");
+				await userManager.AddToRoleAsync(user, role: "Admin");
+			}
+
+			return Ok();
+		}
+	}
+}
---
+++ SimpleBlogCore.WebApp/Areas/Admin/Models/UserViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SimpleBlogCore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleBlogCore.WebApp.Areas.Admin.Models
+{
+	public class UserViewModel
+	{
+		public Guid Id { get; set; }
+
+		public string Username { get; set; }
+
+		public string Email { get; set; }
+
+		public string Role { get; set; }
+
+		public UserViewModel()
+		{
+
+		}
+
+		public UserViewModel(User user, IEnumerable<string> userRoles)
+		{
+			Id = user.Id;
+			Username = user.UserName;
+			Email = user.Email;
+			Role = userRoles.Single();
+		}
+	}
+}
